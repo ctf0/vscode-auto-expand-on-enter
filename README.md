a70bd0634036721a1a3843ed819619d9239594f0
ctf0 <muah003@gmail.com>
2020-02-06 00:19:54 +0200

    013

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
+# Auto Expand On Enter
+
 ![demo](https://user-images.githubusercontent.com/7388088/71270683-dd985c00-235a-11ea-9749-81a78f5f7b9e.gif)
+
+## Features
+
+- smart expand for braces ex.
+    - `SOL|}`, `ES|}` ,`SOL{|`, `someText{|EOL` will add a single new line "normal behavior"
+        > <sub>| == cursor</sub><br>
+        > <sub>SOL == start of line</sub><br>
+        > <sub>EOL == end of line</sub><br>
+        > <sub>ES == empty spaces</sub><br>
+
+- expand method chains
+- expand arguments, array items
+- expand html tags "using emmet `balanceOut` command"
+
+### Notes
+
+- expanding both braces & html tags at the same time wont work, as expanding braces have will take over
--- extension.js
+++ extension.js
@@ -17,95 +17,161 @@ async function activate(context) {
     })
 
     // content expand
-    context.subscriptions.push(
-        vscode.commands.registerCommand('autoExpand.content', async () => {
-            let editor = vscode.window.activeTextEditor
-
-            if (editor) {
-                let { document, selections } = editor
-
-                for (const selection of invertSelections(selections)) {
-                    if (selection.isSingleLine) {
-                        let { start } = selection
-                        let txt = document.lineAt(start.line).text
-                        let length = txt.length
-                        let match = txt.match(/^\s+/)
-                        let space = match && match.length ? match[0] : ''
-
-                        // method chain
-                        txt = txt.replace(new RegExp(/\)\./g), `)${EOL}${space}.`)
-                        // args
-                        txt = txt.replace(new RegExp(/(?<=['"])(\s+)?,(\s+)?['"]/g), (match) => {
-                            return match.replace(/,\s+/, `,${EOL}${space}`)
-                        })
-
-                        await editor.edit(
-                            (edit) => edit.replace(new vscode.Range(start.line, 0, start.line, length), txt),
-                            { undoStopBefore: false, undoStopAfter: false }
-                        )
-                    }
-                }
-
-                vscode.commands.executeCommand('cursorEnd')
-            }
-        })
-    )
+    context.subscriptions.push(vscode.commands.registerCommand('autoExpand.content', expandContent))
 
     // newline expand
-    context.subscriptions.push(
-        vscode.commands.registerCommand('autoExpand.newLine', async () => {
-            let editor = vscode.window.activeTextEditor
-
-            if (editor) {
-                let { selections } = editor
-                let arr = []
-
-                for (let selection of invertSelections(selections)) {
-                    let res = await createSelections(editor, selection)
-
-                    if (res) {
-                        arr.push(...res)
-                    }
-                }
-
-                if (arr.length) {
-                    editor.selections = arr
-                }
-
-                await addNewLine()
-                editor.selections = editor.selections.filter((value, index) => !(index % 2))
-            }
-        })
-    )
+    context.subscriptions.push(vscode.commands.registerCommand('autoExpand.newLine', expandNewLine))
 }
 
+async function expandContent() {
+    let editor = vscode.window.activeTextEditor
+
+    if (editor) {
+        let { document, selections } = editor
+
+        for (const selection of invertSelections(selections)) {
+            if (selection.isSingleLine) {
+                let { start } = selection
+                let txt = document.lineAt(start.line).text
+                let length = txt.length
+                let match = txt.match(/^\s+/)
+                let space = match ? match.join('') : ''
+
+                txt = txt
+                    .replace(new RegExp(/\)\./g), `)${EOL}${space}.`)
+                    .replace(new RegExp(/(?<=\S)(\s+)?,(\s+)?\S/g), (match) => {
+                        return match.replace(/,\s+/, `,${EOL}${space}`)
+                    }).replace(new RegExp(/(?<=['"])(\s+)?,(\s+)?['"]/g), (match) => {
+                        return match.replace(/,\s+/, `,${EOL}${space}`)
+                    })
+
+                await editor.edit(
+                    (edit) => edit.replace(new vscode.Range(start.line, 0, start.line, length), txt),
+                    { undoStopBefore: false, undoStopAfter: false }
+                )
+            }
+        }
+
+        vscode.commands.executeCommand('cursorEnd')
+    }
+}
+
+async function expandNewLine() {
+    let editor = vscode.window.activeTextEditor
+
+    if (editor) {
+        let { selections, document } = editor
+        let { languageId } = document
+        let arr = []
+        selections = invertSelections(selections)
+
+        for (let selection of selections) {
+            let res = await createSelections(editor, selection)
+
+            if (res) {
+                arr.push(...res)
+            }
+        }
+
+        if (arr.length) {
+            editor.selections = arr
+        } else if (config.htmlBasedLangs.includes(languageId)) {
+            let res = await forHtml(editor, selections)
+
+            if (res.length) {
+                arr.push(...res)
+            }
+
+            editor.selections = arr
+        }
+
+        await addNewLine()
+        editor.selections = editor.selections.filter((value, index) => !(index % 2))
+    }
+}
+
+/* Html --------------------------------------------------------------------- */
+async function forHtml(editor, selections) {
+    let { document } = editor
+    let arr = []
+
+    if (selections.some((selection) => checkForHtmlTag(document, selection.end))) {
+        await vscode.commands.executeCommand('editor.emmet.action.balanceOut')
+
+        for (const selection of vscode.window.activeTextEditor.selections) {
+            let { start, end } = selection
+
+            arr.push(...[
+                new vscode.Selection(start, start),
+                new vscode.Selection(end, end)
+            ])
+        }
+    }
+
+    return arr
+}
+
+function checkForHtmlTag(document, end) {
+    let { line, character } = end
+
+    let before = getChar(document, new vscode.Range(line, 0, line, character), />(\s+)?$/)
+    let after = getChar(document, new vscode.Range(line, character, line, document.lineAt(line).text.length), /^(\s+)?<\//)
+
+    let bTrim = before.trim()
+    let aTrim = after.trim()
+
+    // do nothing
+    if (bTrim && bTrim.endsWith('>') && aTrim && aTrim.startsWith('</')) {
+        return false
+    }
+
+    // start or end of tags
+    if (
+        (bTrim && bTrim.endsWith('>')) ||
+        (aTrim && aTrim.startsWith('</'))
+    ) {
+        return true
+    }
+
+    return false
+}
+
+/* Normal ------------------------------------------------------------------- */
 async function createSelections(editor, selection) {
     let { end } = selection
     let { document } = editor
     let result = getCharResult(document, end)
 
-    if (!result.char) {
+    if (!result.hasOwnProperty('char')) {
         return false
     }
 
     // get open & close chars
     let { char, direction, before, after } = result
-    let counter = charsList[char] || open.find((k) => charsList[k] === char)
+    let counter = charsList[char.trim()] || open.find((k) => charsList[k] === char.trim())
     let isRight = direction != 'toLeft'
+    counter = isRight
+        ? resolveCounter(char) + counter
+        : counter + resolveCounter(char)
 
-    let searchIn = getText(isRight, document, end)
+    let searchIn = getText(isRight, document, end, char.length)
     let offset = await getOffset(isRight, searchIn, char, counter)
 
     return getPositions(isRight, document, end, offset, before, after)
 }
 
-/* --------------------------------------------------------------------- */
-function getText(isRight, document, end) {
+function resolveCounter(char) {
+    let match = char.match(/\s+/)
+
+    return match ? match.join('') : ''
+}
+
+function getText(isRight, document, end, len) {
     let { line, character } = end
 
     return isRight
-        ? document.getText(document.validateRange(new vscode.Range(line, character - 1, document.lineCount + 1, 0)))
-        : document.getText(document.validateRange(new vscode.Range(0, 0, line, character + 1)))
+        ? document.getText(document.validateRange(new vscode.Range(line, character - len, document.lineCount + 1, 0)))
+        : document.getText(document.validateRange(new vscode.Range(0, 0, line, character + len)))
 }
 
 function getPositions(isRight, document, end, offset, before, after) {
@@ -115,18 +181,14 @@ function getPositions(isRight, document, end, offset, before, after) {
 
     /**
      * going right & EOL && destination is already on its own line
+     * or going left && SOL
      * then remian at ur position
      */
-    if (isRight && !after && !document.getText(new vscode.Range(pos, pos)).trim()) {
+    if (
+        (isRight && !after && !document.getText(new vscode.Range(pos, pos)).trim()) ||
+        (!isRight && !before)
+    ) {
         return [new vscode.Selection(end, end)]
-    }
-
-    /**
-     * going left && SOL
-     * then go to the other end
-     */
-    if (!isRight && !before) {
-        return [new vscode.Selection(pos, pos)]
     }
 
     // return both ends
@@ -142,7 +204,7 @@ async function getOffset(isRight, txt, char, counter) {
 
     return isRight
         ? await getCharOffsetRight(txt, regex, char)
-        : await getCharOffsetLeft(txt.slice(0, -1), regex, counter)
+        : await getCharOffsetLeft(txt.slice(0, `-${char.length}`), regex, counter)
 }
 
 async function getCharOffsetRight(txt, regex, open) {
@@ -151,7 +213,7 @@ async function getCharOffsetRight(txt, regex, open) {
         let isOpen = 0
 
         txt.replace(new RegExp(regex, 'g'), (match, offset) => {
-            match == open
+            match === open
                 ? isOpen++
                 : isOpen--
 
@@ -181,39 +243,44 @@ async function getCharOffsetLeft(txt, regex, open) {
 }
 
 /* Chars --------------------------------------------------------------------- */
+function getChar(document, range, regex) {
+    let match = document.getText(range).match(regex)
+
+    return match ? match[0] : ''
+}
+
 function getCharResult(document, end) {
     let result = {}
     let { line, character } = end
 
-    let before = document.getText(
-        document.validateRange(new vscode.Range(end, end.with(line, character > 0 ? character - 1 : character)))
-    )
-    let after = document.getText(
-        document.validateRange(new vscode.Range(end, end.with(line, character + 1)))
-    )
+    let before = getChar(document, new vscode.Range(line, 0, line, character), /(\S(\s+)?)$/)
+    let after = getChar(document, new vscode.Range(line, character, line, document.lineAt(line).text.length), /^((\s+)?\S)/)
 
-    if (after && close.includes(after)) {
-        result = isSupported(after)
-    } else if (before && open.includes(before)) {
-        result = isSupported(before)
+    let bTrim = before.trim()
+    let aTrim = after.trim()
+
+    if (aTrim && close.includes(aTrim)) {
+        result = isSupported(aTrim, after)
+    } else if (bTrim && open.includes(bTrim)) {
+        result = isSupported(bTrim, before)
     }
 
-    return Object.assign({}, result, {
+    return Object.assign(result, {
         before: before,
         after: after
     })
 }
 
-function isSupported(char) {
-    let res = open.includes(char)
+function isSupported(toCompare, toUse) {
+    let res = open.includes(toCompare)
         ? 'toRight'
-        : close.includes(char)
+        : close.includes(toCompare)
             ? 'toLeft'
             : false
 
     if (res) {
         return {
-            char: char,
+            char: toUse,
             direction: res
         }
     }
